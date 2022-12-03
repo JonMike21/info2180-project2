afb94bd065daacae9395ffe3daa2dd478259cea5
Jordan <jdwyer@sma.gov.jm>
2022-12-03 17:51:27 -0500

    Added constraints for tables

--- Html&Css/schema.sql
+++ Html&Css/schema.sql
@@ -31,6 +31,10 @@ CREATE TABLE `contacts` (
     PRIMARY KEY(`id`)
 )ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE `contacts`  
+    ADD CONSTRAINT `fk_contacts1` FOREIGN KEY (`assigned_to`) REFERENCES `users` (`id`),  
+    ADD CONSTRAINT `fk_contacts2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
+
 DROP TABLE IF EXISTS `notes`;
 CREATE TABLE `notes` (
     `id` INTEGER AUTO_INCREMENT,
@@ -38,9 +42,12 @@ CREATE TABLE `notes` (
     `comment` TEXT(45) NOT NULL,
     `created_by` INTEGER NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY(`id`) 
 )ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE `notes`  
+    ADD CONSTRAINT `fk_notes_contacts1` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`id`),
+    ADD CONSTRAINT `fk_notes_contacts2` FOREIGN KEY (`created_by`) REFERENCES `contacts` (`id`);
 
 INSERT INTO `users` VALUES 
 (NULL,'John','Brown','pass123','admin@project.com','Admin',CURRENT_TIMESTAMP),
@@ -53,3 +60,8 @@ INSERT INTO `contacts` VALUES
 (NULL,'Manager','Johnny','Brown','Rus@project.com','8994231','CompanyRUs','Sales Lead',2,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
 (NULL,'Manager','Jack','Daniels','Rus@project.com','8994231','CompanyRUs','Sales Lead',2,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
 (NULL,'Asst Manager','Janai','Williams','Rus@project.com','8994231','CompanyRUs','Support',2,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+
+INSERT INTO `notes` VALUES 
+(NULL,1,'Johnny made this note',1,CURRENT_TIMESTAMP),
+(NULL,2,'Jack made this noe',2,CURRENT_TIMESTAMP),
+(NULL,1,'Support is a bad role',1,CURRENT_TIMESTAMP);
