078267d890333d11d7b3fdfa6aaddb430772f8a9
Jon-Michael <jof8888@gmail.com>
2022-12-06 23:54:49 -0500

    add logout

--- Html&Css/note.php
+++ Html&Css/note.php
@@ -69,6 +69,8 @@ if(isset($_POST['SwitchSale']))
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="scripts/createissue.js" type="text/javascript"></script> 
         <script src="notes.js" type="text/javascript"></script> 
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script src="logout.js"></script>
          
     </head>
 
@@ -99,7 +101,7 @@ if(isset($_POST['SwitchSale']))
 
             <div>
                 <i class="fa fa-sign-out"></i>
-                <a href="">Logout</a>
+                <a id="logout" href="UserLogin.html">Logout</a>
             </div>       
 
         </div>
